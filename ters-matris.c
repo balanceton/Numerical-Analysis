9f2c234de38d9924bcfda36af9340d10d398d8ac
Ibrahim Sahin <ibrahim2001sahin@hotmail.com>
2022-05-21 20:39:39 +0300

    done.

--- ters-matris.c
+++ ters-matris.c
@@ -1,114 +1,115 @@
 #include<stdio.h>
 #include <math.h>
 
-int minor(int x,int y,int arr[3][3]){
-	int i,j;
-	int minor[4];
-	int minor2[2][2];
-	int q = 0;
-	for(i=0;i<3;i++){
-		if(i != x){
-		for(j=0;j<3;j++){
-			if(j != y){
-        minor[q] = arr[i][j];
-		q++;
+
+float birimyap(int x,float arr[x][x],float r[x][x]){
+	int i,j,z,w;
+	float q;
+	// 1-) Alt üçgen haline getir
+	for(j=0;j<x;j++){ 
+		z = 1;
+		for(i=0;i<x;i++){
+			if(i != j && i>j){
+					q = arr[i][j]/arr[i-z][j];
+					for(w=0;w<x;w++){
+						arr[i][w] -= arr[i-z][w]*q;
+						r[i][w] -= r[i-z][w]*q;
+					}	
+					z++;
+				}
+			
 			}
 		}
+	
+	// 2-) Köşegenleri 1 yap
+	for(i=0;i<x;i++){ 
+		for(j=0;j<x;j++){
+			if(i==j){
+				q = arr[i][j];
+					for(w=0;w<x;w++){
+						arr[i][w] /= q;
+						r[i][w] /= q;
+					}	
+					
+				}
+			
+			}
 		}
-	}
-	q = 0;
-    for(i=0;i<2;i++){
-		for(j=0;j<2;j++){
-            minor2[i][j] = minor[q];
-			q++; 
+	
+	// 3-) Üst üçgen matrise çevir
+	for(j=x-1;j>=0;j--){ 
+		z = 1;
+		for(i=x-1;i>=0;i--){
+			if(i != j && j>i){
+					q = arr[i][j]/arr[i+z][j];
+					for(w=0;w<x;w++){
+						arr[i][w] -= arr[i+z][w]*q;
+						r[i][w] -= r[i+z][w]*q;
+					}	
+					z++;
+				}
+			
+			}
 		}
+		
+		
+		
+		
+	
 	}
-     return minor2;
-}
+	
+	
 
-int determinant2(int arr[2][2]){
-	int toplam=0;
-	toplam += arr[0][0]*arr[1][1] - arr[0][1]*arr[1][0];
-	return toplam;
-}
-
-int determinant3(int x,int arr[x][x]){ //determinant hesaplama 
-	int i;
-	int det = 0;
-	for(i=0;i<3;i++){
-		det += pow(-1,i)*determinant2(minor(1,i,arr));
-	}
-	return det;
-}
-
-int transpoz(int x,int arr[x][x]){
-	int i,j,a;
-	for(i=0;i<x;i++){
-		for(j=i;j<x;j++){
-          a = arr[i][j];
-		  arr[i][j] = arr[j][i];
-		  arr[j][i] = a;
-		}
-	}
-	return arr;
-}
+	
+	
 
 int main(){
-	int x,i,j,determinant;
-	printf("matrisinizin buyuklugunu giriniz:");
+	int x,i,j;
+	printf("Bilinmeyen sayisi:");
 	scanf("%d",&x);
-	int mat[x][x];
-	int minormat[x][x];
-	printf("Matris elemanlarini giriniz:\n");
+	float birim[x][x];
 	for(i=0;i<x;i++){
 		for(j=0;j<x;j++){
-	    	scanf(" %d",&mat[i][j]);
-	  }
-	    	printf("\n");
-	}
-	
-	// 1-)Transpozu al
-    transpoz(x,mat);
-
-	// 2-)Determinantı hesapla
-    determinant = determinant3(x,mat);
-    printf("\ndeterminant:%d \n",determinant);
-
-	// 3-) Her bir 2x2 minor için det bul
-     for(i=0;i<x;i++){
-		for(j=0;j<x;j++){
-	    	minormat[i][j] = determinant2(minor(i,j,mat));
-	  }
-	}
-
-	// 4-) Kofaktör matrisi oluştur
-	for(i=0;i<x;i++){
-		for(j=0;j<x;j++){
-	    	if((i+j) % 2 != 0){
-				minormat[i][j] *= -1;
+			if(i == j){
+				birim[i][j] = 1;
+			}else{
+				birim[i][j] = 0;
 			}
-	  }
+		}
 	}
-
-	// 5-) Her bir elemanı determinanta böl
-
+	float mat[x][x];
+	float sonuc[x];
+	float kokler[x];
+	printf("Denklemleri girin\n");
 	for(i=0;i<x;i++){
 		for(j=0;j<x;j++){
-	    	minormat[i][j] = minormat[i][j] / determinant;
-	  }
+	      	printf("Sirayla bilinmeyenlerin katsayilari:");
+	      	scanf("%f",&mat[i][j]);
 	}
-
-    for(i=0;i<x;i++){
+	}
+	
+	for(i=0;i<x;i++){
 		for(j=0;j<x;j++){
-	    	 printf(" %d ",minormat[i][j]);
-	  }
-	  printf("\n");
+	      	printf(" %f ",mat[i][j]);
+	}
+	printf("\n");
 	}
 	
+	birimyap(x,mat,birim);
+	printf("\n");
+	printf("\n");
+	printf("Matrisinizin tersi:");
+	printf("\n");
+	for(i=0;i<x;i++){
+		for(j=0;j<x;j++){
+	      	printf(" %f ",birim[i][j]);
+	}
+	printf("\n");
+	}
+	
+	
+	
 
-	
-	
-	
 	
 	return 0;
-	}
+}
